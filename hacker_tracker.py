481f051d60a69d1167855310058125857c203055
redrover9 <grace.thompson97@gmail.com>
2021-10-25 10:15:22 -0700

    add comments for paul

--- hacker_tracker.py
+++ hacker_tracker.py
@@ -3,11 +3,11 @@ import re
 import requests
 from twilio.rest import Client
 
-account_SID = "<SID>"
-auth_token = "<token>"
+account_SID = "<SID>" #replace with SID
+auth_token = "<token>" #replace with authtoken
 twilio_cli = Client(account_SID, auth_token)
-twilio_num = "<num>"
-phone_num = "<num>"
+twilio_num = "<num>" #replace with twilio number
+phone_num = "<num>" #replace with personal number
 
 f = open("auth.log", "r")
 connected_ips = ""
@@ -20,11 +20,11 @@ for line in f:
         connected_ips += connected_ip + " "
 connected_ips = connected_ips.split()
 for ip in connected_ips:
-    if ip != "<ip>" and ip != "<ip>":
+    if ip != "<ip>" and ip != "<ip>": #replace with personal ips
         ip_info = requests.get("http://ip-api.com/json/" + ip)
         python_json = json.loads(ip_info.text)
         location = python_json["city"]
-        message = "Login to <server> from " + location
+        message = "Login to <server> from " + location #replace with server name
         twilio_cli.messages.create(body=message, from_=twilio_num, to=phone_num)
          
 f.close()
