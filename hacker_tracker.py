da3a07a7d12ca2e6bd93ac2e5c6e1279bab53434
redrover9 <grace.thompson97@gmail.com>
2021-10-25 10:07:55 -0700

    remove old code no longer used

--- hacker_tracker.py
+++ hacker_tracker.py
@@ -10,21 +10,14 @@ twilio_num = "+13202881378"
 phone_num = "+12506151971"
 
 f = open("auth.log", "r")
-ips = ""
 connected_ips = ""
 for line in f:
-    has_ip = ""
-    has_ip = re.search("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", line)
-    if has_ip:
-        ip = has_ip.group(0)
-        ips += ip + " "
     has_connected = ""
     has_connected = re.search("Accepted publickey", line)
     if has_connected:
         has_connected_ip = re.search("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", line)
         connected_ip = has_connected_ip.group(0)
         connected_ips += connected_ip + " "
-ips = ips.split()
 connected_ips = connected_ips.split()
 for ip in connected_ips:
     if ip != "199.247.247.70" and ip != "24.244.70.68":
