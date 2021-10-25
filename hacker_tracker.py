1ffeb628a6abdeadadb3051887b2e52b4083a24c
redrover9 <grace.thompson97@gmail.com>
2021-10-25 10:11:04 -0700

    redact pii

--- hacker_tracker.py
+++ hacker_tracker.py
@@ -6,8 +6,8 @@ from twilio.rest import Client
 account_SID = "<SID>"
 auth_token = "<token>"
 twilio_cli = Client(account_SID, auth_token)
-twilio_num = "+13202881378"
-phone_num = "+12506151971"
+twilio_num = "<num>"
+phone_num = "<num>"
 
 f = open("auth.log", "r")
 connected_ips = ""
@@ -20,11 +20,11 @@ for line in f:
         connected_ips += connected_ip + " "
 connected_ips = connected_ips.split()
 for ip in connected_ips:
-    if ip != "199.247.247.70" and ip != "24.244.70.68":
+    if ip != "<ip>" and ip != "<ip>":
         ip_info = requests.get("http://ip-api.com/json/" + ip)
         python_json = json.loads(ip_info.text)
         location = python_json["city"]
-        message = "Login to paul.youthlearntocode.com from " + location
+        message = "Login to <server> from " + location
         twilio_cli.messages.create(body=message, from_=twilio_num, to=phone_num)
          
 f.close()
